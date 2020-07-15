378968c121f0a8c614d2c109dd6b39039e4eaae5
Lucas <lqquyen12894@gmail.com>
2020-07-14 21:36:14 -0400

    Outline layers and TDD

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repositories.dart
@@ -0,0 +1,10 @@
+import 'package:clean_architecture/core/error/failures.dart';
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository{
+  // Return Failure when something wrong
+  // Otherwise return NumberTrivia Object
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number); 
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia(); 
+}
